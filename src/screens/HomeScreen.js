c044a04306c8336172a731a0b9ed6ad1cc36456a
Gabriel Gutierrez <ggutierrez@applaudostudios.com>
2022-02-05 17:51:11 -0400

    styled homeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
+import Container from '../components/Container'
 
 const HomeScreen = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Zemoga React Native Challenge</Text>
-      <Text>By Gabriel Gutierrez</Text>
-      <Text>Github: Gabogg07</Text>
+    <Container style={styles.container}>
 
-      <TouchableWithoutFeedback onPress={()=>navigation.navigate('PostList')}>
-        <View>
-          <Text>START</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    </View>
+      <View style={styles.segment}>
+        <StatusBar style="auto" />
+        <Text style={styles.text}>Zemoga React Native Challenge</Text>
+        <Text style={styles.text}>By Gabriel Gutierrez</Text>
+        <Text style={styles.text}>Github: Gabogg07</Text>
+
+        <TouchableWithoutFeedback onPress={()=>navigation.navigate('PostList')}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>START</Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    </Container>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#ebebeb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  segment:{
+    backgroundColor:'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width:'90%',
+    height:'95%',
+    borderRadius:15,
+  },
+  text:{
+    fontSize:20
+  },
+  buttonText:{
+    fontSize:20,
+    color:'white',
+  },
+  button: {
+    marginVertical:15,
+    padding:10,
+    width:'90%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+    borderRadius:5
+  }
 });
 
 export default HomeScreen
